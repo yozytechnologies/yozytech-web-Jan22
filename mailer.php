6fc5f13f357e2ea1e66203a25dea4026a4d04250
yozytechnologies <75421168+yozytechnologies@users.noreply.github.com>
2022-04-13 18:11:06 +0530

    Add Blog
    Add Blog

--- mailer.php
+++ mailer.php
@@ -1,5 +1,5 @@
 <?php
-    // Only process POST reqeusts.
+    // Only process POST reqeustsg.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
